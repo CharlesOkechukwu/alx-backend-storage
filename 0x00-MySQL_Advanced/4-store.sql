e3b86115b0ee95006c756fede1b49cac7475ad89
CharlesOkechukwu <realcharlieok@gmail.com>
2024-05-09 20:46:06 +0000

    create a trigger for a insert actions on that table

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,10 @@
+-- createing a trigger that decreases the quantity of an item after adding new order
+
+DROP TRIGGER IF EXISTS decrease_qty
+DELIMETER $$
+CREATE TRIGGER decrease_qty AFTER INSERT ON ordersd
+FOR EACH ROW
+BEGIN
+	UPDATE items SET quamtity = quantity - NEW.number WHERE name = NEW.item_name;
+ENG $$
+DELIMETER ;

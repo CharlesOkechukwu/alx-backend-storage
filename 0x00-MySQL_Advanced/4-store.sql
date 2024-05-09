506aea439237870c4efe405d3697850916e7c0c1
CharlesOkechukwu <realcharlieok@gmail.com>
2024-05-09 21:06:18 +0000

    trigger correction

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,11 +1,11 @@
 -- createing a trigger that decreases the quantity of an item after adding new order
 
 DROP TRIGGER IF EXISTS `decrease_qty`;
-DELIMETER $$
+DELIMITER $$
 CREATE TRIGGER `decrease_qty` AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
 	UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
 END;
 $$
-DELIMETER ;
+DELIMITER ;

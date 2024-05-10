b05c1978f0a94a439bf67ec766a8bc4995b3d840
CharlesOkechukwu <realcharlieok@gmail.com>
2024-05-10 02:07:38 +0000

    create a procedure that updates average score of a user

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,8 +3,11 @@ DROP PROCEDURE IF EXISTS `ComputeAverageScoreForUser`;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-	UPDATE users SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id)
-	WHERE id = user_id;
+	DECLARE total INT DEFAULT 0;
+	DECLARE project_count INT DEFAULT 0;
+	SELECT SUM(score) INTO total FROM corrections WHERE corrections.user_id = user_id;
+	SELECT COUNT(*) INTO project_count FROM corrections WHERE corrections.user_id = user_id;
+	UPDATE users SET average_score = total / project_count WHERE users.id = user_id;
 END;
 $$
 DELIMITER ;

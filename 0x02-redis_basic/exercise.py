aeac3d90c84543df550d1169d447ef20244d740d
CharlesOkechukwu <realcharlieok@gmail.com>
2024-05-16 12:19:59 -0700

    create Cache class to store redis instance

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""redis storage module to implement redis storage"""
+import uuid
+import redis
+from typing import Union
+
+
+class Cache:
+    """redis class to store an instance of redis client"""
+    def __init__(self):
+        """initialize class attributes"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, float, int]) -> str:
+        """
+        generate a key using uuid to store data in redis
+        and return key"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key

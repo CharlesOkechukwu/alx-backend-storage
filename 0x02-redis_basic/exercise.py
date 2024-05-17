06cbdf82862c07903ae78b1ee95ba242904d2033
CharlesOkechukwu <realcharlieok@gmail.com>
2024-05-17 07:33:46 -0700

    add get method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """redis storage module to implement redis storage"""
 import uuid
 import redis
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -19,3 +19,24 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable) -> Callable:
+        """accepts key and a callable as argument and returns
+        the value of that key from a redis database"""
+        value = self._redis.get(key)
+        if fn is None:
+            return value
+        else:
+            return fn(value)
+
+    def get_str(self, key: str) -> str:
+        """parameterizes get methon with the callable to convert
+        return value to byte string
+        """
+        return self.get(key, lambda v: v.decode('utf-8'))
+
+    def get_int(self, key: int) -> int:
+        """parameterize get method with the callable to convert
+        return value int
+        """
+        return self.get(key, lambda n: int(n))
